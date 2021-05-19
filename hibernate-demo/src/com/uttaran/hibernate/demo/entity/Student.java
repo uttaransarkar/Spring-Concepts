1dd438476c1a629b2d26f658b997024ba6f506a3
unknown <sarkaruttaran@gmail.com>
2021-05-19 18:19:38 +0530

    modified the Student class

--- hibernate-demo/src/com/uttaran/hibernate/demo/entity/Student.java
+++ hibernate-demo/src/com/uttaran/hibernate/demo/entity/Student.java
@@ -2,6 +2,8 @@ package com.uttaran.hibernate.demo.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,6 +12,7 @@ import javax.persistence.Table;
 public class Student {
 		
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)		//assigning primary id using identity column annotation
 	@Column(name="id")
 	private int id;
 	
@@ -25,7 +28,6 @@ public class Student {
 	public Student() {}
 
 	public Student(String firstName, String lastName, String email) {
-		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
